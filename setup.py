e2e7f1e26c8d97791948c0138edfeaa519540da3
Marco <mvcastro1975@gmail.com>
2022-09-04 00:37:47 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
-setup(name='webservice_hidro',
-      version='0.0.1',
-      description='Download hydrological data from Hidroweb-ANA',
-      author='Marco Castro',
-      author_email='mvcastro1975@gmail.com',
-      url='',
-     )
+
+if __name__ == '__main__':
+      
+      setup()
+
+#     setup(name='webservice_hidro',
+#       version='0.0.1',
+#       description='Download hydrological data from Hidroweb-ANA',
+#       author='Marco Castro',
+#       author_email='mvcastro1975@gmail.com',
+#       url='',
+#      )
