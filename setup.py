77d69f66895b02b247034eed42817359a64093f9
Marco <mvcastro1975@gmail.com>
2023-09-10 14:18:14 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='webservice-hidro',
       author='Marco Castro',
       author_email='mvcastro1975@gmail.com',
       url='',
-      py_modules=[]
+      py_modules=['webservice_hidro', ]
  )
